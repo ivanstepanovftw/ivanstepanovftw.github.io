d79456d3619555a8366d777ecdb483c52f8492f1
Ivan Stepanov <ivanstepanovftw@gmail.com>
2025-04-03 21:02:58 +0200

    testing

--- matrix-calculus/en-US/b.sh
+++ matrix-calculus/en-US/b.sh
@@ -4,14 +4,24 @@ ROOT_DIR=$( (cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P) )
 
 temp="$(mktemp -d)"
 output="$ROOT_DIR"
+index_filename="index.qmd"
 
 (
   cd ../arXiv-2501.14787v1 || exit 1
-  pandoc main.tex --wrap=preserve --standalone -Mmath=true -Mlang=en-US --embed-resources=true -t markdown -o "$temp/index.md" --extract-media="$temp"
+  set --
+  set -- "$@" --wrap=preserve
+  set -- "$@" --standalone
+  set -- "$@" -Mlang=en-US
+  set -- "$@" --embed-resources=true
+#  set -- "$@" -t markdown
+#  set -- "$@" -t markdown_mmd
+  set -- "$@" -t commonmark_x
+  set -- "$@" -o "$temp/$index_filename" --extract-media="$temp"
+  pandoc main.tex "$@" || exit 1
   # Add '{% raw %}' after frontmatter and '{% endraw %}' at the end
-  perl -i -pe 'if (/^---$/) { $count++; $_ .= "\n{% raw %}" if $count==2 }' "$temp/index.md"
-  #sed -i -e '1s/^/{% raw %}\n/' "$temp/index.md"
-  sed -i -e '$s/$/\n{% endraw %}/' "$temp/index.md"
+  perl -i -pe 'if (/^---$/) { $count++; $_ .= "\n{% raw %}" if $count==2 }' "$temp/$index_filename"
+  #sed -i -e '1s/^/{% raw %}\n/' "$temp/$index_filename"
+  sed -i -e '$s/$/\n{% endraw %}/' "$temp/$index_filename"
 ) || exit 1
 
 convert_asset() {
@@ -48,11 +58,11 @@ convert_asset() {
 export -f convert_asset
 export output temp
 
-cp "$temp/index.md" "$output/index.md" || exit 1
-find "$temp" -type f -not -name 'index.md' -exec bash -c 'convert_asset "$0" "$1"' {} "$output/index.md" ';'
-#find "$temp" -type f -not -name 'index.md' \
-#  | parallel --no-notice --jobs 4 convert_asset {} "$output/index.md"
-#find "$temp" -type f -not -name 'index.md' -print0 \
-#  | xargs -0 -n 1 -P 4 bash -c 'convert_asset "$0" "$1"' {} "$output/index.md"
+cp "$temp/$index_filename" "$output/$index_filename" || exit 1
+find "$temp" -type f -not -name "$index_filename" -exec bash -c 'convert_asset "$0" "$1"' {} "$output/$index_filename" ';'
+#find "$temp" -type f -not -name "$index_filename" \
+#  | parallel --no-notice --jobs 4 convert_asset {} "$output/$index_filename"
+#find "$temp" -type f -not -name "$index_filename" -print0 \
+#  | xargs -0 -n 1 -P 4 bash -c 'convert_asset "$0" "$1"' {} "$output/$index_filename"
 
 rm -rf "$temp"
